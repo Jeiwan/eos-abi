2b603cfa9fdb58f422a26e52edca9dd6a15fdec4
Ivan Kuznetsov <me@jeiwan.ru>
2018-07-18 12:19:22 +0700

    Decode 'eosio'

--- name_test.go
+++ name_test.go
@@ -9,8 +9,10 @@ import (
 
 func TestNameToString(t *testing.T) {
 	name, _ := hex.DecodeString("a01861fc499b8969")
-
 	str := nameToString(name)
-
 	assert.Equal(t, "ha4tqmjwg4ge", str)
+
+	name, _ = hex.DecodeString("0000000000ea3055")
+	str = nameToString(name)
+	assert.Equal(t, "eosio", str)
 }
